d84140526a999a903b0224865ca4b0be3b3096f0
Nicole Byqvist <nicole.byqvist@mailbox.org>
2024-10-24 19:02:40 -0500

    Fix big laptop again

--- targets/korone.ts
+++ targets/korone.ts
@@ -3,14 +3,14 @@ import { baseConfig } from '../default';
 export const ctx = {
   ...baseConfig,
   font: {
-    family: 'FiraCode Nerd Font Mono',
+    family: 'BlexMono Nerd Font Mono',
     style: 'Light',
-    size: 16,
+    size: 18,
   },
   includedOutput: {
     height: 1600,
     width: 2560,
-    scale: 1.125,
+    scale: 1,
   },
   useGuix: false,
   useAsdf: true,
