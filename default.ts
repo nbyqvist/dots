b214831cdf12f8cd35028fae2cdfc2668b3681d6
Nicole Byqvist <nicole.byqvist@mailbox.org>
2024-10-29 20:02:30 -0500

    Split out theme

--- default.ts
+++ default.ts
@@ -1,31 +1,9 @@
+import github_light from "./themes/github_light";
 import type { Context } from "./types";
 
 export const baseConfig: Context = {
   // Github light color scheme
-  colors: {
-    fg: "#24292f",
-    bg: "#ffffff",
-    base: {
-      black: "#24292e",
-      red: "#d73a49",
-      green: "#28a745",
-      yellow: "#dbab09",
-      blue: "#005cc5",
-      magenta: "#5a32a3",
-      cyan: "#0598bc",
-      white: "#6a737d",
-    },
-    bright: {
-      black: "#959da5",
-      red: "#cb2431",
-      green: "#22863a",
-      yellow: "#b08800",
-      blue: "#005cc5",
-      magenta: "#5a32a3",
-      cyan: "#3192aa",
-      white: "#d1d5da",
-    },
-  },
+  colors: github_light,
   font: {
     size: 16,
     family: "FiraCode Nerd Font Mono",
