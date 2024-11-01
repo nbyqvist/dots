b62594407cdefd3533524335bbe1038b6355f1cb
Nicole Byqvist <nicole.byqvist@mailbox.org>
2024-11-01 15:46:39 -0500

    Add desktop 2

---
+++ targets/tsumugi.ts
@@ -0,0 +1,19 @@
+import { baseConfig } from '../default';
+import { theme } from '../themes/github_light';
+
+export const ctx = {
+  ...baseConfig,
+  colors: theme,
+  font: {
+    ...baseConfig.font,
+    size: 15,
+  },
+  includedOutput: {
+    height: 1200,
+    width: 1920,
+    scale: 1,
+  },
+  rightClickScroll: false,
+  vscodeZoom: 0,
+  useAsdf: true,
+};
