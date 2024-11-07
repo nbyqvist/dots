44d9b6ac0edb3bc525221eaf6dc14b5ed3ec9184
Nicole Byqvist <nbyqvist@mailbox.org>
2024-11-06 20:53:15 -0600

    Add desktop

---
+++ targets/ceres.ts
@@ -0,0 +1,19 @@
+import { baseConfig } from '../default';
+import { theme } from '../themes/github_ultradark';
+
+export const ctx = {
+  ...baseConfig,
+  colors: theme,
+  font: {
+    ...baseConfig.font,
+    size: 15,
+  },
+  includedOutput: {
+    height: 1440,
+    width: 5120,
+    scale: 1,
+  },
+  rightClickScroll: true,
+  vscodeZoom: 0,
+  useAsdf: false,
+};
