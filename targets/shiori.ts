ff16fb79f0480733ac5fac1089e44c3514227eff
Nicole Byqvist <nicole.byqvist@mailbox.org>
2024-11-27 21:24:07 -0600

    Add macbook

---
+++ targets/shiori.ts
@@ -0,0 +1,14 @@
+import { baseConfig } from '../default.ts';
+
+export const ctx = {
+  ...baseConfig,
+  font: {
+    family: 'BlexMono Nerd Font Mono',
+    style: 'Light',
+    size: 14,
+  },
+  helixBin: 'hx',
+  useGuix: false,
+  useAsdf: false,
+  useDirenv: false,
+};
